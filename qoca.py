05c71efc9c6de01ffe2d62bf8aeecfcd948869df
sclang16 <samlan2016@gmail.com>
2021-04-06 16:19:53 -0400

    An attempt was made at the Hamiltonian layer

--- qoca.py
+++ qoca.py
@@ -3,10 +3,12 @@ from typing import Optional, List
 from qiskit import QuantumRegister, QuantumCircuit
 from qiskit.circuit import Gate, Parameter
 from qiskit.qasm import pi
+from qiskit.extensions import UnitaryGate
 
 from qiskit.aqua import aqua_globals
-from qiskit.aqua.components.initial_states import InitialState, Zero
+from qiskit.aqua.components.initial_states import InitialState, Custom
 from qiskit.aqua.components.variational_forms import VariationalForm
+from qiskit.aqua.operators.evolutions import MatrixEvolution
 
 from qiskit.chemistry.fermionic_operator import FermionicOperator
 from qiskit.aqua.operators import WeightedPauliOperator, Z2Symmetries
@@ -19,16 +21,20 @@ import numpy as np
 
 class QOCA(VariationalForm):
     def __init__(self,
+        ferm_operator: Optional[FermionicOperator] = None,
         num_qubits: Optional[int] = 4,
         reps: int = 1,
-        initial_state: Optional[InitialState] = Zero,
-        qubit_mapping: str = 'parity') -> None:
+        initial_state: Optional[InitialState] = None,
+        qubit_mapping: str = 'jordan_wigner') -> None:
         
         super().__init__()
 
         self._num_qubits = num_qubits
         self._reps = reps
-        self._initial_state = initial_state
+        if initial_state is None:
+            self._initial_state = Custom(num_qubits, state='uniform')
+        else:
+            self._initial_state = initial_state
         self._support_parameterized_circuit = True
 
         self._num_parameters = 2 * self._num_qubits * self._reps
@@ -37,11 +43,18 @@ class QOCA(VariationalForm):
         parameters = self.parameters
         self._bounds = [(-np.pi, np.pi) for _ in range(self._num_parameters)]
 
+        if ferm_operator is None:
+            self._ferm_operator = FermionicOperator(h1=None, h2=None)
+        else:
+            self._ferm_operator = ferm_operator
 
-        # Qubit mapping requirements?
-        # How to get num_qubits, since it likely will depend on H^?
-        # What is being treated as H^?
-        # Pick operator evolution for H^ (likely MatrixEvolution) - in constructor or subfunction?
+    def add_hamiltonian_layer(self, circuit: QuantumCircuit, fermionic_operator: FermionicOperator):
+        
+        qubitOp = self._ferm_operator.mapping(map_type = "jordan_wigner", threshold=0.00000001)
+        evol = MatrixEvolution().convert(operator = qubitOp)
+        circuit.append(UnitaryGate(qubitOp, label = None),[0,qubitOp.num_qubits],[])
+
+        return circuit
     
     def add_drive_layer(self, circuit: QuantumCircuit, layer_params: List[Parameter]):
         
@@ -72,6 +85,7 @@ class QOCA(VariationalForm):
         for layer in range(self._reps):
             layer_end_index = int(self._num_parameters/self._reps)
             layer_params = parameters[(layer*layer_end_index):((layer+1)*layer_end_index)]
+            self.add_hamiltonian_layer(circuit, self._ferm_operator)
             self.add_drive_layer(circuit, layer_params)
 
         return circuit
